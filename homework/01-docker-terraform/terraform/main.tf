2d5b46694160077dcae2ad3f19c29bca2d5ed35c
Michael Joshua Vargas <vargas.michaeljoshua@gmail.com>
2026-01-23 22:15:24 +0800

    Added GPC bucket and Big QUery dataset

--- homework/01-docker-terraform/terraform/main.tf
+++ homework/01-docker-terraform/terraform/main.tf
@@ -1,25 +1,47 @@
- # Connect to gcp using ADC (identity verification)
- provider "google" {
-   project = var.project
-   region  = var.region
-   zone    = var.zone
- }
+# Connect to gcp using ADC (identity verification)
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
 
- # add these data blocks 
- 
- # This data source gets a temporary token for the service account
- data "google_service_account_access_token" "default" {
-   provider               = google
-   target_service_account = "terraform-runner@intense-sled-344301.iam.gserviceaccount.com"
-   scopes                 = ["https://www.googleapis.com/auth/cloud-platform"]
-   lifetime               = "3600s"
- }
- 
- # This second provider block uses that temporary token and does the real work
- provider "google" {
-   alias        = "impersonated"
-   access_token = data.google_service_account_access_token.default.access_token
-   project      = var.project
-   region       = var.region
-   zone         = var.zone
- }
+# add these data blocks 
+
+# This data source gets a temporary token for the service account
+data "google_service_account_access_token" "default" {
+  provider               = google
+  target_service_account = "terraform-runner@intense-sled-344301.iam.gserviceaccount.com"
+  scopes                 = ["https://www.googleapis.com/auth/cloud-platform"]
+  lifetime               = "3600s"
+}
+
+# This second provider block uses that temporary token and does the real work
+provider "google" {
+  alias        = "impersonated"
+  access_token = data.google_service_account_access_token.default.access_token
+  project      = var.project
+  region       = var.region
+  zone         = var.zone
+}
+
+# GCP bucket
+resource "google_storage_bucket" "demo-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+# Big Query Dataset
+resource "google_bigquery_dataset" "demo-dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
--- homework/01-docker-terraform/terraform/variable.tf
+++ homework/01-docker-terraform/terraform/variable.tf
@@ -12,3 +12,18 @@ variable "zone" {
   description = "Project zone"
   default     = "us-central1-a"
 }
+
+variable "location" {
+  description = "Project location"
+  default     = "US"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "intense-sled-344301-terra-bucket"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery dataset name"
+  default     = "demo_dataset"
+}
